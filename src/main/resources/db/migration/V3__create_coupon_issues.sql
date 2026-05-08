d561f25ce3d115300f52ca24c9d0a2bdd6ab7d1e
wooqqq <sge0225@naver.com>
2026-05-08 15:22:36 +0900

    fix: coupon_issues sql 파일 내 coupon 테이블명 수정

--- src/main/resources/db/migration/V3__create_coupon_issues.sql
+++ src/main/resources/db/migration/V3__create_coupon_issues.sql
@@ -3,6 +3,6 @@ CREATE TABLE coupon_issues (
     coupon_id  BIGINT   NOT NULL,
     user_id    BIGINT   NOT NULL,
     reg_dt     DATETIME NOT NULL,
-    CONSTRAINT fk_coupon_issue_coupon FOREIGN KEY (coupon_id) REFERENCES coupon (id),
+    CONSTRAINT fk_coupon_issue_coupon FOREIGN KEY (coupon_id) REFERENCES coupons (id),
     CONSTRAINT fk_coupon_issue_user   FOREIGN KEY (user_id)   REFERENCES users (id)
 );
