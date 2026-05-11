b900aadfc15601b2eda47167e95b46d329d5eee6
wooqqq <sge0225@naver.com>
2026-05-11 10:29:45 +0900

    feat: 비관적 락으로 동시성 문제 해결

--- src/main/java/com/example/couponSystem/coupon/repository/CouponRepository.java
+++ src/main/java/com/example/couponSystem/coupon/repository/CouponRepository.java
@@ -1,7 +1,17 @@
 package com.example.couponSystem.coupon.repository;
 
 import com.example.couponSystem.coupon.entity.Coupon;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.id = :id")
+    Optional<Coupon> findByIdWithPessimisticLock(@Param("id") Long id);
 }
